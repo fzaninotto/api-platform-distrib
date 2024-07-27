025c6ec3ddcb85803f6f82420f29320c6f06dfa9
François Zaninotto <fzaninotto@gmail.com>
2024-07-27 12:41:36 +0200

    use default react-admin layout, add resource icons

--- pwa/components/admin/App.tsx
+++ pwa/components/admin/App.tsx
@@ -1,4 +1,8 @@
 import { HydraAdmin, ResourceGuesser } from "@api-platform/admin";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import ReviewsIcon from "@mui/icons-material/Reviews";
+import { Layout, defaultLightTheme, defaultDarkTheme } from "react-admin";
+
 import { BookList } from "./books/BookList";
 import { BookEdit } from "./books/BookEdit";
 import { ReviewList } from "./reviews/ReviewList";
@@ -7,18 +11,27 @@ import { ReviewEdit } from "./reviews/ReviewEdit";
 import { BookShow } from "./books/BookShow";
 
 export const App = () => (
-  <HydraAdmin entrypoint={window.origin}>
+  <HydraAdmin
+    entrypoint={window.origin}
+    layout={Layout}
+    theme={defaultLightTheme}
+    darkTheme={defaultDarkTheme}
+  >
     <ResourceGuesser
       name="books"
       list={BookList}
       show={BookShow}
       edit={BookEdit}
+      recordRepresentation="name"
+      icon={AutoStoriesIcon}
     />
     <ResourceGuesser
       name="reviews"
       list={ReviewList}
       show={ReviewShow}
       edit={ReviewEdit}
+      recordRepresentation="id"
+      icon={ReviewsIcon}
     />
   </HydraAdmin>
 );
